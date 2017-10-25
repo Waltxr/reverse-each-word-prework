df5af536c91a980158143f3422389f7c2f997b82
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-25 16:10:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(sentence)
 
   words.join(" ")
 end
+
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+
+  words.collect do |w|
+    w.reverse!
+  end
+
+  words.join(" ")
+end
