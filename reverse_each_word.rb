30f05dc6c0a40d647fd7767ac9e2bf3b8a52631c
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-25 16:11:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+
+  words.each do |w|
+    w.reverse!
+  end
+
+  words.join(" ")
+end
+
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+
+  words.collect do |w|
+    w.reverse!
+  end
+
+  words.join(" ")
+end
